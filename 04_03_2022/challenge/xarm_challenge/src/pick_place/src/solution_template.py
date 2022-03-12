2565f7e9b9fa8e2f195b72ecd573ee19fbedb0de
BryanMqz <A01562119@itesm.mx>
2022-03-12 23:56:20 +0000

    First update

--- 04_03_2022/challenge/xarm_challenge/src/pick_place/src/solution_template.py
+++ 04_03_2022/challenge/xarm_challenge/src/pick_place/src/solution_template.py
@@ -6,7 +6,8 @@ import tf2_ros
 import moveit_commander
 import moveit_msgs.msg
 from moveit_commander.conversions import pose_to_list
-from geometry_msgs.msg import PoseStamped, Pose
+#from geometry_msgs.msg import PoseStamped, Pose
+import geometry_msgs.msg
 from path_planner.srv import *
 from tf.transformations import *
 from moveit_msgs.msg import Grasp
@@ -14,15 +15,90 @@ from moveit_msgs.msg import Grasp
 class Planner():
 
   def __init__(self):
-    #TODO: Initialise move it interface
+    #http://docs.ros.org/en/kinetic/api/moveit_tutorials/html/doc/move_group_python_interface/move_group_python_interface_tutorial.html
+    
+    #TOdO: First initialize moveit_commander and a rospy node:
+    moveit_commander.roscpp_initialize(sys.argv)
+    rospy.init_node('GoalPlanner', anonymous=True)
 
-  def wait_for_state_update(self,box_name, box_is_known=False, box_is_attached=False, timeout=0.5):
+    #Instantiate a RobotCommander object. 
+    #This object is the outer-level interface to the robot:
+    robot = moveit_commander.RobotCommander()
+    #Instantiate a PlanningSceneInterface object. 
+    #This object is an interface to the world surrounding the robot:
+    scene = moveit_commander.PlanningSceneInterface()
 
-    #TODO: Whenever we change something in moveit we need to make sure that the interface has been updated properly
+    ## Instantiate a `MoveGroupCommander`_ object.  This object is an interface
+    ## to one group of joints.  In this case the group is the joints in the xarm6's
+    ## arm so we set ``group_name = xArm6``. If you are using a different robot,
+    ## you should change this value to the name of your robot arm planning group.
+    ## This interface can be used to plan and execute motions on the xArm:
+    group_name = "xarm6" #| xarm6 |
+    group = moveit_commander.MoveGroupCommander(group_name)
+
+    #We create a DisplayTrajectory publisher which is used 
+    #later to publish trajectories for RViz to visualize:
+    display_trajectory_publisher = rospy.Publisher('/move_group/display_planned_path',
+                                               moveit_msgs.msg.DisplayTrajectory,
+                                               queue_size=20)
+    # Sometimes for debugging it is useful to print the entire state of the
+    # robot:
+    print ("======= Printing robot state =====")
+    print (robot.get_current_state())
+    print ("")
+
+    # Misc variables
+    #self.box_name = ''
+    self.robot = robot
+    self.scene = scene
+    self.group = group
+    self.move_group = move_group
+    self.display_trajectory_publisher = display_trajectory_publisher
+
+
+  def wait_for_state_update(self, box_is_known=False, box_is_attached=False, timeout=0.5):
+
+    #TOdO: Whenever we change something in moveit we need to make sure that the interface has been updated properly
+    box_name = self.box_name
+    scene = self.scene
+
+    ## BEGIN_SUB_TUTORIAL wait_for_scene_update
+    ##
+    ## Ensuring Collision Updates Are Received
+    ## ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
+    ## If the Python node dies before publishing a collision object update message, the message
+    ## could get lost and the box will not appear. To ensure that the updates are
+    ## made, we wait until we see the changes reflected in the
+    # ``get_attached_objects()`` and ``get_known_object_names()`` lists.
+    ## For the purpose of this tutorial, we call this function after adding,
+    ## removing, attaching or detaching an object in the planning scene. We then wait
+    ## until the updates have been made or ``timeout`` seconds have passed
+    start = rospy.get_time()
+    seconds = rospy.get_time()
+    while (seconds - start < timeout) and not rospy.is_shutdown():
+        # Test if the box is in attached objects
+        attached_objects = scene.get_attached_objects([box_name])
+        is_attached = len(attached_objects.keys()) > 0
+
+        # Test if the box is in the scene.
+        # Note that attaching the box will remove it from known_objects
+        is_known = box_name in scene.get_known_object_names()
+
+        # Test if we are in the expected state
+        if (box_is_attached == is_attached) and (box_is_known == is_known):
+            return True
+
+        # Sleep so that we give other threads time on the processor
+        rospy.sleep(0.1)
+        seconds = rospy.get_time()
+
+    # If we exited the while loop without returning then we timed out
+    return False
+    ## END_SUB_TUTORIAL
 
   def addObstacles(self):
 
-    #TODO: Add obstables in the world
+    #TOdO: Add obstacles in the world
 
 	
     #Cargo names
@@ -36,18 +112,74 @@ class Planner():
 
   def goToPose(self,pose_goal):
 
-    #TODO: Code used to move to a given position using move it
+    #TODO: Code used to move to a given position using move it  
+    # Copy class variables to local variables to make the web tutorials more clear.
+    # In practice, you should use the class variables directly unless you have a good
+    # reason not to.
+    move_group = self.move_group
 
+    ## BEGIN_SUB_TUTORIAL plan_to_pose
+    ##
+    ## Planning to a Pose Goal
+    ## ^^^^^^^^^^^^^^^^^^^^^^^
+    ## We can plan a motion for this group to a desired pose for the
+    ## end-effector:
+    pose_goal = geometry_msgs.msg.Pose()
+    pose_goal.orientation.w = 1.0
+    pose_goal.position.x = 0.4
+    pose_goal.position.y = 0.1
+    pose_goal.position.z = 0.4
 
-  def detachBox(self,box_name):
+    move_group.set_pose_target(pose_goal)
 
-  #TODO: Open the gripper and call the service that releases the box
+    ## Now, we call the planner to compute the plan and execute it.
+    plan = move_group.go(wait=True)
+    # Calling `stop()` ensures that there is no residual movement
+    move_group.stop()
+    # It is always good to clear your targets after planning with poses.
+    # Note: there is no equivalent function for clear_joint_value_targets()
+    move_group.clear_pose_targets()
 
+    ## END_SUB_TUTORIAL
 
-  def attachBox(self,box_name):
+  def detachBox(self,box_name, timeout=1):
+    #TODO: Open the gripper and call the service that releases the box
+    # Copy class variables to local variables to make the web tutorials more clear.
+    # In practice, you should use the class variables directly unless you have a good
+    # reason not to.
+    box_name = self.box_name
+    scene = self.scene
+    eef_link = self.eef_link
 
-  #TODO: Close the gripper and call the service that releases the box
+    ## BEGIN_SUB_TUTORIAL detach_object
+    ##
+    ## Detaching Objects from the Robot
+    ## We can also detach and remove the object from the planning scene:
+    scene.remove_attached_object(eef_link, name=box_name)
+    ## END_SUB_TUTORIAL
 
+    # We wait for the planning scene to update.
+    return self.wait_for_state_update(
+        box_is_known=True, box_is_attached=False, timeout=timeout)
+
+  def attachBox(self, box_name, timeout=1):
+
+    #TOdO: Close the gripper and call the service that releases the box
+    ##Attach the box to the robot. The function doesn't check if the robot is near the box or not.
+    ##The robot should first be moved in position before attaching the box.
+    box_name = self.box_name
+    robot = self.robot
+    scene = self.scene
+    eef_link = self.eef_link
+    group_names = self.group_names
+
+    ## Attach te box to the robot
+    grasping_group = 'xarm6'
+    touch_links = robot.get_link_names(group=grasping_group)
+    scene.attach_box(eef_link, box_name, touch_links=touch_links)
+
+    # Wait for the planning scene to update.
+    return self.wait_for_state_update(box_is_attached=True, box_is_known=False, timeout=timeout)
 
 
 class myNode():
