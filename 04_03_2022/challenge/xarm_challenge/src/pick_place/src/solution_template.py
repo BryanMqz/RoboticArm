1b4bccba6a55248d868696e6068369e11a0c6b4f
Kevin Vega <a01612430@tec.mx>
2022-03-14 13:13:16 -0600

    Done basic pick_place

--- 04_03_2022/challenge/xarm_challenge/src/pick_place/src/solution_template.py
+++ 04_03_2022/challenge/xarm_challenge/src/pick_place/src/solution_template.py
@@ -1,206 +1,192 @@
 #!/usr/bin/env python
-from shutil import move
 import rospy
-import sys
-import tf_conversions
 import tf2_ros
-import tf
-import moveit_commander
-import moveit_msgs.msg
-from moveit_commander.conversions import pose_to_list
-from geometry_msgs.msg import PoseStamped, Pose
-from path_planner.srv import *
-from tf.transformations import *
-from moveit_msgs.msg import Grasp
+import numpy as np
+from geometry_msgs.msg import PoseStamped,TransformStamped
+from gazebo_msgs.msg import ModelState,ModelStates
+from path_planner.srv import RequestGoal,RequestGoalResponse,AttachObject,AttachObjectResponse
+from gazebo_ros_link_attacher.srv import Attach, AttachRequest, AttachResponse
 
-class Planner():
+class StateGatherer():
+    def __init__(self):
 
-  def __init__(self):
-    moveit_commander.roscpp_initialize(sys.argv)
+	# We set up the names of each of the target frames that we want to pick or place 
+        rospy.init_node('TaskPlanner')
 
-    #Instantiate a RobotCommander object. 
-    #This object is the outer-level interface to the robot:
-    robot = moveit_commander.RobotCommander()
-    #Instantiate a PlanningSceneInterface object. 
-    #This object is an interface to the world surrounding the robot:
-    scene = moveit_commander.PlanningSceneInterface()
+        self.targets = ["RedBox",
+                        "BlueBox",
+                        "GreenBox",
+                        "DepositBoxRed",
+                        "DepositBoxBlue",
+                        "DepositBoxGreen"]
 
-    ## Instantiate a `MoveGroupCommander`_ object.  This object is an interface
-    ## to one group of joints.  In this case the group is the joints in the xarm6's
-    ## arm so we set ``group_name = xArm6``. If you are using a different robot,
-    ## you should change this value to the name of your robot arm planning group.
-    ## This interface can be used to plan and execute motions on the xArm:
-    group_name = "xarm6" #| xarm6 |
-    move_group = moveit_commander.MoveGroupCommander(group_name)
-    gripper_name = "xarm_gripper"
-    gripper_move = moveit_commander.MoveGroupCommander(gripper_name)
+        self.targets_place = ["DepositBoxRed",
+                              "DepositBoxBlue",
+                              "DepositBoxGreen"]
 
-    #We create a DisplayTrajectory publisher which is used 
-    #later to publish trajectories for RViz to visualize:
-    display_trajectory_publisher = rospy.Publisher('/move_group/display_planned_path',
-                                               moveit_msgs.msg.DisplayTrajectory,
-                                               queue_size=20)
-    # Sometimes for debugging it is useful to print the entire state of the
-    # robot:
-    # print ("======= Printing robot state =====")
-    # print (robot.get_current_state())
-    # print ("")
+        self.targets_pick = ["RedBox",
+                             "BlueBox",
+                             "GreenBox"]
 
-    # Misc variables
-    #self.box_name = ''
-    self.robot = robot
-    self.scene = scene
-    self.move_group = move_group
-    self.display_trajectory_publisher = display_trajectory_publisher
+        # Variables used to iterate through the targets
+        self.task_id = 0
+        self.place_id = 0
+        self.target_pose = PoseStamped()
+        self.status = 0
+        self.bool_changed = 0
+        self.base_updated = 0
 
+        self.attached = {"RedBox": False,
+                         "BlueBox": False,
+                         "GreenBox": False,
+                         "None": False}
+        self.frame = "None"
 
-  def wait_for_state_update(self,box_name, box_is_known=False, box_is_attached=False, timeout=0.5):
-    
-    #TODO: Whenever we change something in moveit we need to make sure that the interface has been updated properly
-    pass
+        # Transform handlers
+        self.br = tf2_ros.TransformBroadcaster()
+        self.brStatic = tf2_ros.StaticTransformBroadcaster()
 
-  def addObstacles(self):
+        # Service publisher
+        rospy.Service('RequestGoal', RequestGoal, self.sendGoal)
 
-    #TODO: Add obstables in the world
+        # We want to wait until Gazebo services are up before continuing with the code
+        rospy.wait_for_service('/gazebo/set_link_state')
+        rospy.Service('AttachObject', AttachObject, self.AttachObject)
+        self.goal_handler = rospy.ServiceProxy('RequestGoal', RequestGoal)
 
-	
-    #Cargo names
-    targets = ["RedBox",
-               "BlueBox",
-               "GreenBox"]
-    #goal names
-    boxes = ["DepositBoxGreen",
-               "DepositBoxRed",
-               "DepositBoxBlue"]
+        self.attach_srv = rospy.ServiceProxy('/link_attacher_node/attach', Attach)
+        self.deattach_srv = rospy.ServiceProxy('/link_attacher_node/detach', Attach)
 
-  def goToPose(self,pose_goal):
-    
-    #TODO: Code used to move to a given position using move it
-    self.robot.get_current_state()
-    group_name = "xarm6" #| xarm6 |
-    move_group = moveit_commander.MoveGroupCommander(group_name)
-    # x = pose_goal.transform.translation.x
-    # y = pose_goal.transform.translation.y
-    # z = pose_goal.transform.translation.z
+        # Add tf2 handlers
+        self.tfBuffer = tf2_ros.Buffer()
+        self.listener = tf2_ros.TransformListener(self.tfBuffer)
 
-    # qx = pose_goal.transform.rotation.x
-    # qy = pose_goal.transform.rotation.y
-    # qz = pose_goal.transform.rotation.z
-    # qw = pose_goal.transform.rotation.w
+        # Add publishers and subscribers
+        self.pub = rospy.Publisher('/gazebo/set_model_state', ModelState, queue_size=10)
+        rospy.Subscriber("gazebo/model_states", ModelStates, self.callback)
 
-    pose_g = Pose()
-    pose_g.orientation.w = pose_goal.transform.rotation.w
-    pose_g.position.x = pose_goal.transform.translation.x
-    pose_g.position.y = pose_goal.transform.translation.y
-    pose_g.position.z = pose_goal.transform.translation.z
-    pose_g.orientation.x = pose_goal.transform.rotation.x
-    pose_g.orientation.y = pose_goal.transform.rotation.y
-    pose_g.orientation.z = pose_goal.transform.rotation.z
-    
-    move_group.set_pose_reference_frame('sensor_frame')
-    move_group.set_num_planning_attempts(100)
-    move_group.set_planning_time(15)
-    # move_group.set_end_effector_link('link_attach')
-    # move_group.set_pose_target([pose_g.position.x,pose_g.position.y,pose_g.position.z,0.717037,0.697036,0,0])
-    move_group.set_pose_target(pose_g)
-    plan = move_group.go(wait=True)
-    # Calling `stop()` ensures that there is no residual movement
-    move_group.stop()
-    # It is always good to clear your targets after planning with poses.
-    # Note: there is no equivalent function for clear_joint_value_targets()
-    move_group.clear_pose_targets()
+        t = TransformStamped()
+        t.header.stamp = rospy.Time.now()
+        t.header.frame_id = "xarm_gripper_base_link"
+        t.child_frame_id = "link_attach"
+        t.transform.translation.z = 0.14 # Distance to be tuned manually so that collisions are prevented calibrate to improve the visuals
+        t.transform.rotation.x = 0.717
+        t.transform.rotation.y = 0.697
+
+        self.brStatic.sendTransform(t)
 
 
-  def detachBox(self):
+    def AttachObject(self, data):
+
+        # Function to attach the object, basically the box will follow the ee with a certain offset
+        self.frame = data.frame
+        self.attached[self.frame] = data.action
+        self.object = data.frame
+        self.bool_changed = 1
+        return AttachObjectResponse(True)
 
 
-  #TODO: Open the gripper and call the service that releases the box
-    pass
-    gripper_name = "xarm_gripper"
-    gripper_move = moveit_commander.MoveGroupCommander(gripper_name)
-    gripper_move.set_named_target('open')
-    gripper_move.go(wait=True)
+    def main(self):
+
+        # If there's an object attached to the ee we want it to follow its trajectory
+        while not rospy.is_shutdown():
+
+            if self.attached[self.frame] and self.bool_changed:
+
+                req = AttachRequest()
+                req.model_name_1 = "xarm6"
+                req.link_name_1 = "link6"
+                req.model_name_2 = self.object
+                req.link_name_2 = "link"
+
+                self.attach_srv.call(req)
+
+            elif self.bool_changed:
+
+                req = AttachRequest()
+                req.model_name_1 = "xarm6"
+                req.link_name_1 = "link6"
+                req.model_name_2 = self.object
+                req.link_name_2 = "link"
+
+                self.deattach_srv.call(req)
 
 
-  def attachBox(self):
+    def callback(self, data):
 
-  #TODO: Close the gripper and call the service that releases the box
-    gripper_name = "xarm_gripper"
-    gripper_move = moveit_commander.MoveGroupCommander(gripper_name)
-    gripper_move.set_named_target('close')
-    gripper_move.go(wait=True)
+        # Map Gazebo and TF
+        aux_idx = data.name.index(self.targets[self.task_id])
+        self.target_pose.pose = data.pose[aux_idx]
 
+        # We uptdate tf2 frames so that they have the same postiion as in Gazebo, if the object is attached we will keep
+        # the same position relative to the ee.
+        for target in self.targets:
 
+            if (target != self.frame) or not (self.attached[target]):
+                aux_idx = data.name.index(target)
+                t = TransformStamped()
+                t.header.stamp = rospy.Time.now()
+                t.header.frame_id = "sensor_frame"
+                t.child_frame_id = target
+                t.transform.translation = data.pose[aux_idx].position
+                t.transform.rotation = data.pose[aux_idx].orientation
+                self.br.sendTransform(t)
 
-class myNode():
-  def __init__(self):
-    #TODO: Initialise ROS and create the service calls
-    rospy.init_node('ch_node')
-    # Good practice trick, wait until the required services are online before continuing with the aplication
-    rospy.wait_for_service('RequestGoal')
-    rospy.wait_for_service('AttachObject')
-
-
-    
-
-  def getGoal(self,action):
-    print("Inside getgoal")
-    getgoal = rospy.ServiceProxy('RequestGoal',RequestGoal)
-    
-    goal = getgoal(action)
-    # print(goal)
-    return goal
-    #TODO: Call the service that will provide you with a suitable target for the movement
+            else:
+                # print("Attached")
+                tf = self.tfBuffer.lookup_transform("sensor_frame", "link_attach", rospy.Time(), rospy.Duration(0.5))
+                tf.child_frame_id = target
+                tf.header.stamp = rospy.Time.now()
+                self.br.sendTransform(tf)
 
 
-  def tf_goal(self, goal):
-    #Listener del atacch frame para sacar la transformada con respecto al goal or from the sensor frame
-    #Al parecer si es respecto al link base
-    #TODO:Use tf2 to retrieve the position of the target with respect to the proper reference frame
-    print("inside TF")
-    tf_buffer = tf2_ros.Buffer(rospy.Duration(2.0))
-    tf2_ros.TransformListener(tf_buffer)
+    def sendGoal(self,req):
 
-    try:
-      transformation = tf_buffer.lookup_transform('sensor_frame', goal , rospy.Time(0), rospy.Duration(0.1))
-      transformation2 = tf_buffer.lookup_transform('xarm_gripper_base_link','link_attach' , rospy.Time(0), rospy.Duration(0.1))
-    except (tf2_ros.LookupException, tf2_ros.ConnectivityException,
-            tf2_ros.ExtrapolationException):
-        print('Something went wrong')
-    # print(transformation)
-    # print(transformation2)
-    transformation.transform.rotation =transformation2.transform.rotation
-    return transformation
+        # Proposed state machine to control the goals (currently 1)
+        if req.action == "place":
+            if self.status == 1:
+                try:
+                    aux_pose = self.targets_place[self.place_id]
+                    self.place_id += 1
+                    status = True
+                    self.status = 0
 
+                except:
+                    aux_pose = "End"
+                    status = True
 
-  def main(self):
-    #TODO: Main code that contains the aplication
-    self.planner = Planner()
-    self.planner.addObstacles()
-    for i in range(3):
-      goal = self.getGoal("pick")
-      print("Our goal is:",goal.goal)
-      goal_pose = self.tf_goal(goal.goal)
-      self.planner.goToPose(goal_pose)
+            else:
+                status = False
+                aux_pose = "None, invalid place request! "
 
-      #attach
-      self.planner.attachBox()
-      print('Starting place task')
+            print(aux_pose)
+            return RequestGoalResponse(aux_pose,status)
 
-      goal = self.getGoal("place")
-      goal_pose = self.tf_goal(goal.goal)
-      goal_pose.transform.translation.z += 0.2
-      self.planner.goToPose(goal_pose)
-      self.planner.detachBox()
+        elif req.action == "pick":
+            if self.status == 0:
+                try:
+                    aux_pose = self.targets_pick[self.task_id]
+                    self.task_id += 1
+                    status = True
+                    self.status = 1
 
-    rospy.signal_shutdown("Task Completed")
+                except:
+                    status = True
+                    aux_pose = "End"
 
+            else:
+                status = False
+                aux_pose = "None, invalid pick request! "
 
+            print("target: " + aux_pose)
+            return RequestGoalResponse(aux_pose,status)
 
 if __name__ == '__main__':
-  try:
-    node = myNode()
-    node.main()
 
-  except rospy.ROSInterruptException:
-    pass
+    try:
+        aux = StateGatherer()
+        aux.main()
+
+    except (rospy.ROSInterruptException, rospy.ROSException("topic was closed during publish()")):
+        pass
+
