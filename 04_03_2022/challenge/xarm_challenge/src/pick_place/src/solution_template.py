da26c5b97237f2b78b1292d8e75816484cb9c35a
Kevin Vega <a01612430@tec.mx>
2022-03-14 10:19:20 -0600

    Only need to attach the object

--- 04_03_2022/challenge/xarm_challenge/src/pick_place/src/solution_template.py
+++ 04_03_2022/challenge/xarm_challenge/src/pick_place/src/solution_template.py
@@ -1,6 +1,5 @@
 #!/usr/bin/env python
-from operator import ge
-from re import T
+from shutil import move
 import rospy
 import sys
 import tf_conversions
@@ -18,7 +17,6 @@ class Planner():
 
   def __init__(self):
     moveit_commander.roscpp_initialize(sys.argv)
-    rospy.init_node('GoalPlanner', anonymous=True)
 
     #Instantiate a RobotCommander object. 
     #This object is the outer-level interface to the robot:
@@ -33,7 +31,9 @@ class Planner():
     ## you should change this value to the name of your robot arm planning group.
     ## This interface can be used to plan and execute motions on the xArm:
     group_name = "xarm6" #| xarm6 |
-    group = moveit_commander.MoveGroupCommander(group_name)
+    move_group = moveit_commander.MoveGroupCommander(group_name)
+    gripper_name = "xarm_gripper"
+    gripper_move = moveit_commander.MoveGroupCommander(gripper_name)
 
     #We create a DisplayTrajectory publisher which is used 
     #later to publish trajectories for RViz to visualize:
@@ -42,15 +42,14 @@ class Planner():
                                                queue_size=20)
     # Sometimes for debugging it is useful to print the entire state of the
     # robot:
-    print ("======= Printing robot state =====")
-    print (robot.get_current_state())
-    print ("")
+    # print ("======= Printing robot state =====")
+    # print (robot.get_current_state())
+    # print ("")
 
     # Misc variables
     #self.box_name = ''
     self.robot = robot
     self.scene = scene
-    self.group = group
     self.move_group = move_group
     self.display_trajectory_publisher = display_trajectory_publisher
 
@@ -75,21 +74,61 @@ class Planner():
                "DepositBoxBlue"]
 
   def goToPose(self,pose_goal):
-
+    
     #TODO: Code used to move to a given position using move it
-    moveit_commander.move_group.MoveGroupCommander.
+    self.robot.get_current_state()
+    group_name = "xarm6" #| xarm6 |
+    move_group = moveit_commander.MoveGroupCommander(group_name)
+    # x = pose_goal.transform.translation.x
+    # y = pose_goal.transform.translation.y
+    # z = pose_goal.transform.translation.z
+
+    # qx = pose_goal.transform.rotation.x
+    # qy = pose_goal.transform.rotation.y
+    # qz = pose_goal.transform.rotation.z
+    # qw = pose_goal.transform.rotation.w
+
+    pose_g = Pose()
+    pose_g.orientation.w = pose_goal.transform.rotation.w
+    pose_g.position.x = pose_goal.transform.translation.x
+    pose_g.position.y = pose_goal.transform.translation.y
+    pose_g.position.z = pose_goal.transform.translation.z
+    pose_g.orientation.x = pose_goal.transform.rotation.x
+    pose_g.orientation.y = pose_goal.transform.rotation.y
+    pose_g.orientation.z = pose_goal.transform.rotation.z
+    
+    move_group.set_pose_reference_frame('sensor_frame')
+    move_group.set_num_planning_attempts(100)
+    move_group.set_planning_time(15)
+    # move_group.set_end_effector_link('link_attach')
+    # move_group.set_pose_target([pose_g.position.x,pose_g.position.y,pose_g.position.z,0.717037,0.697036,0,0])
+    move_group.set_pose_target(pose_g)
+    plan = move_group.go(wait=True)
+    # Calling `stop()` ensures that there is no residual movement
+    move_group.stop()
+    # It is always good to clear your targets after planning with poses.
+    # Note: there is no equivalent function for clear_joint_value_targets()
+    move_group.clear_pose_targets()
 
 
-  def detachBox(self,box_name):
+  def detachBox(self):
+
 
   #TODO: Open the gripper and call the service that releases the box
     pass
+    gripper_name = "xarm_gripper"
+    gripper_move = moveit_commander.MoveGroupCommander(gripper_name)
+    gripper_move.set_named_target('open')
+    gripper_move.go(wait=True)
 
 
-  def attachBox(self,box_name):
+  def attachBox(self):
 
   #TODO: Close the gripper and call the service that releases the box
-    pass
+    gripper_name = "xarm_gripper"
+    gripper_move = moveit_commander.MoveGroupCommander(gripper_name)
+    gripper_move.set_named_target('close')
+    gripper_move.go(wait=True)
 
 
 
@@ -123,11 +162,14 @@ class myNode():
     tf2_ros.TransformListener(tf_buffer)
 
     try:
-      transformation = tf_buffer.lookup_transform(goal,'link_base', rospy.Time(0), rospy.Duration(0.1))
+      transformation = tf_buffer.lookup_transform('sensor_frame', goal , rospy.Time(0), rospy.Duration(0.1))
+      transformation2 = tf_buffer.lookup_transform('xarm_gripper_base_link','link_attach' , rospy.Time(0), rospy.Duration(0.1))
     except (tf2_ros.LookupException, tf2_ros.ConnectivityException,
             tf2_ros.ExtrapolationException):
         print('Something went wrong')
-    print(transformation)
+    # print(transformation)
+    # print(transformation2)
+    transformation.transform.rotation =transformation2.transform.rotation
     return transformation
 
 
@@ -135,11 +177,21 @@ class myNode():
     #TODO: Main code that contains the aplication
     self.planner = Planner()
     self.planner.addObstacles()
-    goal = self.getGoal("pick")
-    print("Our goal is:",goal.goal)
-    tf = self.tf_goal(goal.goal)
+    for i in range(3):
+      goal = self.getGoal("pick")
+      print("Our goal is:",goal.goal)
+      goal_pose = self.tf_goal(goal.goal)
+      self.planner.goToPose(goal_pose)
 
-    self.getGoal("place")
+      #attach
+      self.planner.attachBox()
+      print('Starting place task')
+
+      goal = self.getGoal("place")
+      goal_pose = self.tf_goal(goal.goal)
+      goal_pose.transform.translation.z += 0.2
+      self.planner.goToPose(goal_pose)
+      self.planner.detachBox()
 
     rospy.signal_shutdown("Task Completed")
 
