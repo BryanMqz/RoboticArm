6fc92c84e29e6023b5d8c7122165cc24d1ae4baf
Kevin Vega <a01612430@tec.mx>
2022-03-13 19:58:26 -0600

    Services and tf working in solution

--- 04_03_2022/challenge/xarm_challenge/src/pick_place/src/solution_template.py
+++ 04_03_2022/challenge/xarm_challenge/src/pick_place/src/solution_template.py
@@ -1,13 +1,15 @@
 #!/usr/bin/env python
+from operator import ge
+from re import T
 import rospy
 import sys
 import tf_conversions
 import tf2_ros
+import tf
 import moveit_commander
 import moveit_msgs.msg
 from moveit_commander.conversions import pose_to_list
-#from geometry_msgs.msg import PoseStamped, Pose
-import geometry_msgs.msg
+from geometry_msgs.msg import PoseStamped, Pose
 from path_planner.srv import *
 from tf.transformations import *
 from moveit_msgs.msg import Grasp
@@ -15,9 +17,6 @@ from moveit_msgs.msg import Grasp
 class Planner():
 
   def __init__(self):
-    #http://docs.ros.org/en/kinetic/api/moveit_tutorials/html/doc/move_group_python_interface/move_group_python_interface_tutorial.html
-    
-    #TOdO: First initialize moveit_commander and a rospy node:
     moveit_commander.roscpp_initialize(sys.argv)
     rospy.init_node('GoalPlanner', anonymous=True)
 
@@ -56,49 +55,14 @@ class Planner():
     self.display_trajectory_publisher = display_trajectory_publisher
 
 
-  def wait_for_state_update(self, box_is_known=False, box_is_attached=False, timeout=0.5):
-
-    #TOdO: Whenever we change something in moveit we need to make sure that the interface has been updated properly
-    box_name = self.box_name
-    scene = self.scene
-
-    ## BEGIN_SUB_TUTORIAL wait_for_scene_update
-    ##
-    ## Ensuring Collision Updates Are Received
-    ## ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
-    ## If the Python node dies before publishing a collision object update message, the message
-    ## could get lost and the box will not appear. To ensure that the updates are
-    ## made, we wait until we see the changes reflected in the
-    # ``get_attached_objects()`` and ``get_known_object_names()`` lists.
-    ## For the purpose of this tutorial, we call this function after adding,
-    ## removing, attaching or detaching an object in the planning scene. We then wait
-    ## until the updates have been made or ``timeout`` seconds have passed
-    start = rospy.get_time()
-    seconds = rospy.get_time()
-    while (seconds - start < timeout) and not rospy.is_shutdown():
-        # Test if the box is in attached objects
-        attached_objects = scene.get_attached_objects([box_name])
-        is_attached = len(attached_objects.keys()) > 0
-
-        # Test if the box is in the scene.
-        # Note that attaching the box will remove it from known_objects
-        is_known = box_name in scene.get_known_object_names()
-
-        # Test if we are in the expected state
-        if (box_is_attached == is_attached) and (box_is_known == is_known):
-            return True
-
-        # Sleep so that we give other threads time on the processor
-        rospy.sleep(0.1)
-        seconds = rospy.get_time()
-
-    # If we exited the while loop without returning then we timed out
-    return False
-    ## END_SUB_TUTORIAL
+  def wait_for_state_update(self,box_name, box_is_known=False, box_is_attached=False, timeout=0.5):
+    
+    #TODO: Whenever we change something in moveit we need to make sure that the interface has been updated properly
+    pass
 
   def addObstacles(self):
 
-    #TOdO: Add obstacles in the world
+    #TODO: Add obstables in the world
 
 	
     #Cargo names
@@ -112,98 +76,70 @@ class Planner():
 
   def goToPose(self,pose_goal):
 
-    #TODO: Code used to move to a given position using move it  
-    # Copy class variables to local variables to make the web tutorials more clear.
-    # In practice, you should use the class variables directly unless you have a good
-    # reason not to.
-    move_group = self.move_group
+    #TODO: Code used to move to a given position using move it
+    moveit_commander.move_group.MoveGroupCommander.
 
-    ## BEGIN_SUB_TUTORIAL plan_to_pose
-    ##
-    ## Planning to a Pose Goal
-    ## ^^^^^^^^^^^^^^^^^^^^^^^
-    ## We can plan a motion for this group to a desired pose for the
-    ## end-effector:
-    pose_goal = geometry_msgs.msg.Pose()
-    pose_goal.orientation.w = 1.0
-    pose_goal.position.x = 0.4
-    pose_goal.position.y = 0.1
-    pose_goal.position.z = 0.4
 
-    move_group.set_pose_target(pose_goal)
+  def detachBox(self,box_name):
 
-    ## Now, we call the planner to compute the plan and execute it.
-    plan = move_group.go(wait=True)
-    # Calling `stop()` ensures that there is no residual movement
-    move_group.stop()
-    # It is always good to clear your targets after planning with poses.
-    # Note: there is no equivalent function for clear_joint_value_targets()
-    move_group.clear_pose_targets()
+  #TODO: Open the gripper and call the service that releases the box
+    pass
 
-    ## END_SUB_TUTORIAL
 
-  def detachBox(self,box_name, timeout=1):
-    #TODO: Open the gripper and call the service that releases the box
-    # Copy class variables to local variables to make the web tutorials more clear.
-    # In practice, you should use the class variables directly unless you have a good
-    # reason not to.
-    box_name = self.box_name
-    scene = self.scene
-    eef_link = self.eef_link
+  def attachBox(self,box_name):
 
-    ## BEGIN_SUB_TUTORIAL detach_object
-    ##
-    ## Detaching Objects from the Robot
-    ## We can also detach and remove the object from the planning scene:
-    scene.remove_attached_object(eef_link, name=box_name)
-    ## END_SUB_TUTORIAL
+  #TODO: Close the gripper and call the service that releases the box
+    pass
 
-    # We wait for the planning scene to update.
-    return self.wait_for_state_update(
-        box_is_known=True, box_is_attached=False, timeout=timeout)
-
-  def attachBox(self, box_name, timeout=1):
-
-    #TOdO: Close the gripper and call the service that releases the box
-    ##Attach the box to the robot. The function doesn't check if the robot is near the box or not.
-    ##The robot should first be moved in position before attaching the box.
-    box_name = self.box_name
-    robot = self.robot
-    scene = self.scene
-    eef_link = self.eef_link
-    group_names = self.group_names
-
-    ## Attach te box to the robot
-    grasping_group = 'xarm6'
-    touch_links = robot.get_link_names(group=grasping_group)
-    scene.attach_box(eef_link, box_name, touch_links=touch_links)
-
-    # Wait for the planning scene to update.
-    return self.wait_for_state_update(box_is_attached=True, box_is_known=False, timeout=timeout)
 
 
 class myNode():
   def __init__(self):
     #TODO: Initialise ROS and create the service calls
-
+    rospy.init_node('ch_node')
     # Good practice trick, wait until the required services are online before continuing with the aplication
     rospy.wait_for_service('RequestGoal')
     rospy.wait_for_service('AttachObject')
 
-  def getGoal(self,action):
 
+    
+
+  def getGoal(self,action):
+    print("Inside getgoal")
+    getgoal = rospy.ServiceProxy('RequestGoal',RequestGoal)
+    
+    goal = getgoal(action)
+    # print(goal)
+    return goal
     #TODO: Call the service that will provide you with a suitable target for the movement
 
 
   def tf_goal(self, goal):
-
+    #Listener del atacch frame para sacar la transformada con respecto al goal or from the sensor frame
+    #Al parecer si es respecto al link base
     #TODO:Use tf2 to retrieve the position of the target with respect to the proper reference frame
+    print("inside TF")
+    tf_buffer = tf2_ros.Buffer(rospy.Duration(2.0))
+    tf2_ros.TransformListener(tf_buffer)
+
+    try:
+      transformation = tf_buffer.lookup_transform(goal,'link_base', rospy.Time(0), rospy.Duration(0.1))
+    except (tf2_ros.LookupException, tf2_ros.ConnectivityException,
+            tf2_ros.ExtrapolationException):
+        print('Something went wrong')
+    print(transformation)
+    return transformation
 
 
   def main(self):
     #TODO: Main code that contains the aplication
     self.planner = Planner()
     self.planner.addObstacles()
+    goal = self.getGoal("pick")
+    print("Our goal is:",goal.goal)
+    tf = self.tf_goal(goal.goal)
+
+    self.getGoal("place")
 
     rospy.signal_shutdown("Task Completed")
 
